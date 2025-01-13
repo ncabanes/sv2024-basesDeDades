54b2aec6c4b521fa3fcfc38672cc10d37954c287
Nacho Cabanes <ncabanes@users.noreply.github.com>
2025-01-13 17:38:15 +0100

    04.026: Repàs 5 SQL (empleats - enunciat)

---
+++ 0405-DDL-DML/04-026-repas5-empleats.sql
@@ -0,0 +1,22 @@
+-- 1. Noms dels empleats el codi de departament dels quals siga el 11, ordenats 
+-- alfabèticament.
+
+-- 2. Nom i codi dels empleats el nom dels quals comença per "A".
+
+-- 3. Nom dels empleats per als quals no coneixem el departament.
+
+-- 4 i 5. Codi i nom dels departaments el codi dels quals està entre 12 i 20, 
+-- de 2 formes diferents.
+
+-- 6. Quantitat d'empleats.
+
+-- 7. Codis de departament en els quals realment hi ha empleats, sense duplicats.
+
+-- 8. Codi de cada departament que apareix en la taula d'empleats, juntament 
+-- amb la quantitat d'empleats que conté.
+
+-- 9. Codi de cada departament que apareix en la taula d'empleats, juntament 
+-- amb la quantitat d'empleats que conté, però només per als departaments amb 2 o 
+-- més empleats.
+
+-- 10. Nom de cada empleat i nom del departament en el qual treballa.
