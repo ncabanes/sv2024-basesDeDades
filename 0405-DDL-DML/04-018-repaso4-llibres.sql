0ec553b7bbaaf12d5d630a296a58daf283f4890a
Nacho Cabanes <ncabanes@users.noreply.github.com>
2025-01-13 17:31:19 +0100

    04.018: Repàs 4 SQL (llibres - enunciat)

--- 0405-DDL-DML/04-018-repaso4-llibres.sql
+++ 0405-DDL-DML/04-018-repaso4-llibres.sql
@@ -1,6 +1,7 @@
 -- Crea una primera versió d'una primera base de dades de llibres. Per a cada 
 -- llibre voldrem un codi numèric, un títol, un autor, un número de pàgines i una 
 -- ubicació.
+
 CREATE TABLE llibres
 (
     codi NUMERIC(10) PRIMARY KEY,
@@ -19,26 +20,45 @@ INSERT INTO llibres VALUES (5,'La metamorfosis','Franz Kafka',1500,'e1b3');
 INSERT INTO llibres VALUES (6,'América','Franz Kafka',900,'e1b4');
 
 -- Mostra els llibres que continguen la paraula "poema".
-
+SELECT * FROM llibres WHERE titol LIKE '%Poema%';
 
 -- Mostra els llibres dels quals no tinguem anotada la ubicació.
-
+SELECT * FROM llibres WHERE ubicacio IS NULL;
 
 -- Mostra autor i títol dels llibres que tinguen entre 1000 i 2000 pàgines.
+SELECT autor, titol FROM llibres WHERE numeroPagines BETWEEN 1000 AND 2000;
 
+SELECT autor, titol 
+FROM llibres 
+WHERE numeroPagines >= 1000 AND numeroPagines <= 2000;
 
 -- Mostra els llibres de l'autor "Franz Kafka" que estiguen en la ubicació "e1b2" o en "e1b3".
-
+SELECT * FROM llibres WHERE autor IS 'Franz Kafka' AND (ubicacio='e1b2' OR ubicacio='e1b3');
 
 -- Mostra la mitjana de pàgines que tenen els nostres llibres.
-
+SELECT AVG(numeroPagines)
+FROM llibres;
 
 -- Mostra la llista d'ubicacions, sense duplicats, ordenada.
+SELECT DISTINCT ubicacio
+FROM llibres
+ORDER BY ubicacio;
 
+-- Títol del llibre, nom de l'autor i número de pàgines, ordenat en primer 
+-- lloc per autor i després del que té més pàgines al qual té menys.
+
+SELECT titol, autor, numeroPagines 
+FROM llibres 
+ORDER BY autor ASC, numeroPagines DESC;
 
 -- Mostra el nom de cada autor, juntament amb la quantitat de llibres que tenim seus.
-
+SELECT autor, COUNT(*)
+FROM llibres
+GROUP BY autor;
 
 -- Mostra el nom de cada autor, juntament amb la quantitat de llibres que 
 -- tenim seus, però només per als autors dels quals tinguem 3 o més llibres.
-
+SELECT autor, COUNT(titol) AS totalLlibres
+FROM llibres
+GROUP BY autor
+HAVING totalLlibres >= 3;
