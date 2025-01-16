70f4418793df1944fe07178ad85736b8e6f2b392
Nacho Cabanes <ncabanes@users.noreply.github.com>
2025-01-16 16:14:53 +0100

    04.035: Repàs 6 SQL (city+country)

---
+++ 0405-DDL-DML/04-035-repas6-city.sql
@@ -0,0 +1,118 @@
+-- 1. Nom de les ciutats que continguen la seqüència de lletres "san", potser amb majúscules distintes (per exemple "Santa Fe"), ordenades.
+
+
+
+-- 2. Nom de les ciutats que continguen una "i" després d'una "e", i esta després d'una "a" (per exemple, "Berazategui").
+
+
+
+-- 3. Nom de la ciutat i nom del país, per a les ciutats que tinguen entre 900.000 habitants i un milió d'habitants (de formes distintes).
+
+
+
+-- 4. Nom de cada ciutat i nom del país al qual pertany, ordenat per ciutat i, en cas que dos ciutats s'anomenen igual, per nom del país.
+
+
+
+-- 5. Nom dels països dels quals no tenim cap ciutat, ordenats per població de manera descendent.
+
+
+
+-- 6. Països el nom dels quals coincidix amb el d'una ciutat (repte: seràs capaç d'aconseguir-ho d'almenys quatre formes distintes?).
+
+
+
+-- 7. Nom de cada país al costat del nom de la seua capital (el codi apareix en el camp "Capital"), si la capital apareix en la nostra base de dades, ordenat per país.
+
+
+
+-- 8. País, capital i governant, per als països el governant dels quals sapiem, ordenat per país.
+
+
+
+-- 9. Capitals i noms de països, el producte interior brut (GNP) dels quals siga superior a la mitjana dels d'Europa.
+
+
+
+-- 10. Per a cada país: nom, població i suma de les poblacions de les ciutats que coneixem d'eixe país.
+
+
+
+-- 11. Països, la població dels quals és menys de la mitat de la ciutat més poblada de tota la base de dades.
+
+
+
+-- 12. Capitals que tinguen la mitat (o més) de la població del seu país.
+
+
+
+-- 13. Ciutats el nom de les quals està repetit.
+
+
+
+-- 14. Nom dels països que tenen ciutats el nom de les quals està repetit.
+
+
+
+
+
+-- ---------------------------------
+
+
+
+
+
+-- 1. Nombre de las ciudades que contengan la secuencia de letras "san", quizá con mayúsculas distintas (por ejemplo "Santa Fe"), ordenadas.
+
+
+
+-- 2. Nombre de las ciudades que contengan una "i" después de una "e", y ésta después de una "a" (por ejemplo, "Berazategui").
+
+
+
+-- 3. Nombre de la ciudad y nombre del país, para las ciudades que tengan entre 900.000 habitantes y un millón de habitantes (de formas distintas).
+
+
+
+-- 4. Nombre de cada ciudad y nombre del país al que pertenece, ordenado por ciudad y , en caso de que dos ciudades se llamen igual, por nombre del país.
+
+
+
+-- 5. Nombre de los países de los que no tenemos ninguna ciudad, ordenados por población de forma descendente.
+
+
+
+-- 6. Países cuyo nombre coincide con el de una ciudad (reto: ¿serás capaz de conseguirlo de no menos de cuatro formas distintas?).
+
+
+
+-- 7. Nombre de cada país junto al nombre de su capital (el código aparece en el campo "Capital"), si la capital aparece en nuestra base de datos, ordenado por país.
+
+
+
+-- 8. País, capital y gobernante, para los países cuyo gobernante sepamos, ordenado por país.
+
+
+
+-- 9. Capitales y nombres de países cuyo producto interior bruto sea superior a la media de los de Europa.
+
+
+
+-- 10. Para cada país: nombre, población y suma de las poblaciones de las ciudades que conocemos de ese país.
+
+
+
+-- 11. Países cuya población es menos de la mitad de la ciudad más poblada de toda la base de datos.
+
+
+
+-- 12. Capitales que tengan la mitad (o más) de la población de su país.
+
+
+
+-- 13. Ciudades cuyo nombre está repetido.
+
+
+
+-- 14. Nombre de los países que tienen ciudades cuyo nombre está repetido.
+
