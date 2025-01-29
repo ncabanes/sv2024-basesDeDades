8d29838335588e237302606d194238555b80718f
Nacho Cabanes <ncabanes@users.noreply.github.com>
2025-01-29 12:46:13 +0100

    04.054: Repàs 9 SQL (empleats, part 1, solució)

--- 0405-DDL-DML/04-054-Repas9-EmpleatsDepartaments1.sql
+++ 0405-DDL-DML/04-054-Repas9-EmpleatsDepartaments1.sql
@@ -49,43 +49,144 @@ INSERT INTO empleado VALUES(13, '82635162B', 'Juan Antonio','Sáez', 'Guerrero',
 -- 1. Lista el primer apellido de los empleados eliminando los apellidos que 
 -- estén repetidos.
 
+SELECT DISTINCT apellido1
+FROM empleado;
+
 
 -- 2. Lista el nombre y apellidos de los empleados en una única columna.
+
+SELECT nombre || ' ' || apellido1 || ' ' || apellido2 AS nombreCompleto
+FROM empleado;
+
+-- No en Oracle ("demasiados parámetros" para un CONCAT<<)
+
+SELECT CONCAT(nombre, ' ', apellido1, ' ', apellido2) AS nombreCompleto
+FROM empleado;
 
 
 -- 3. Lista el nombre de cada departamento y el dinero del que dispone. Para 
 -- calcular este dato habrá que restar al valor del presupuesto inicial (columna 
 -- "presupuesto") los gastos que se han generado (columna gastos).
 
+SELECT nombre, presupuesto - gastos AS dineroDisponible
+FROM departamento; 
+
 
 -- 4. Lista los apellidos y el nombre de todos los empleados, ordenados de 
 -- forma alfabética tendiendo en cuenta en primer lugar sus apellidos y luego su 
 -- nombre.
 
+SELECT apellido2, apellido1, nombre
+FROM empleado
+ORDER BY apellido2, apellido1, nombre;
+
 
 -- 5. Devuelve una lista con el nombre y el presupuesto, de los 3 departamentos 
 -- que tienen mayor presupuesto.
 
+-- Oracle
 
--- 6. Devuelve una lista con el nombre de los departamentos y el presupesto, de 
+SELECT nombre, presupuesto
+FROM departamento
+ORDER BY presupuesto DESC
+FETCH FIRST 3 ROWS ONLY;
+
+-- SQLite
+
+SELECT nombre, presupuesto
+FROM departamento
+ORDER BY presupuesto DESC
+LIMIT 3; 
+
+
+-- 6. Devuelve una lista con el nombre de los departamentos y el presupuesto, de 
 -- aquellos que tienen un presupuesto entre 100000 y 200000 euros. Hazlo dos 
 -- veces: primero con el operador BETWEEN y luego sin él.
+
+SELECT nombre, presupuesto
+FROM departamento
+WHERE presupuesto BETWEEN 100000 AND 200000; 
+
+SELECT nombre, presupuesto
+FROM departamento
+WHERE presupuesto >= 100000 AND presupuesto <= 200000; 
 
 
 -- 7. Lista todos los datos de los empleados cuyo segundo apellido no 
 -- conozcamos.
+
+SELECT *
+FROM empleado
+WHERE apellido2 IS NULL; 
 
 
 -- 8. Lista todos los datos de los empleados cuyo segundo apellido sea Díaz o 
 -- Moreno. Hazlo de 2 formas: primero sin utilizar el operador IN, y después 
 -- utilizando dicho operador.
 
+SELECT *
+FROM empleado
+WHERE apellido2 = 'Díaz' OR apellido2 = 'Moreno'; 
+
+SELECT *
+FROM empleado
+WHERE apellido2 IN ('Díaz','Moreno');
+
 
 -- 9. Devuelve un listado con los apellido1, apellido2 y nombre de los 
 -- empleados, junto con el nombre de los departamentos donde trabaja cada uno. 
 -- Hazlo de 2 formas: primero con INNER JOIN y luego con WHERE.
 
+SELECT apellido1, apellido2, 
+  empleado.nombre, departamento.nombre AS dpto
+FROM departamento INNER JOIN empleado
+ON departamento.id = id_departamento;
+
+SELECT apellido1, apellido2, 
+  empleado.nombre, departamento.nombre AS dpto
+FROM empleado, departamento
+WHERE departamento.id = id_departamento; 
+
 
 -- 10. Devuelve un listado con el identificador y el nombre del departamento, 
 -- solamente de aquellos departamentos que tienen empleados (de al menos 4 formas distintas).
 
+SELECT DISTINCT departamento.id, departamento.nombre 
+FROM departamento INNER JOIN empleado
+ON departamento.id = id_departamento;
+
+SELECT DISTINCT departamento.id, departamento.nombre 
+FROM empleado, departamento
+WHERE departamento.id = id_departamento; 
+
+SELECT DISTINCT id, nombre 
+FROM departamento WHERE id IN 
+(
+    SELECT id_departamento FROM empleado
+);
+
+SELECT DISTINCT id, nombre 
+FROM departamento WHERE id = ANY 
+(
+    SELECT id_departamento FROM empleado
+);
+
+SELECT DISTINCT id, nombre 
+FROM departamento WHERE EXISTS
+(
+    SELECT id_departamento FROM empleado
+    WHERE empleado.id_departamento = departamento.id
+);
+
+-- Otras dos alternativas menos razonables (innecesariamente complejas)
+
+SELECT DISTINCT departamento.id, departamento.nombre 
+FROM departamento LEFT JOIN empleado
+ON departamento.id = id_departamento
+WHERE empleado.nombre IS NOT NULL;
+
+SELECT departamento.id, departamento.nombre 
+FROM departamento LEFT JOIN empleado
+ON departamento.id = id_departamento
+GROUP BY departamento.id, departamento.nombre
+HAVING COUNT(empleado.nombre) > 0;
